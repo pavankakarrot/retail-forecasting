83505b4f16ab3712cc18b74b9c5ee6d84c5e7e52
pavankakarrot <pavankakarrot@gmail.com>
2024-12-04 14:32:09 +0000

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+# Import main classes for easier access
+from .preprocessing import RetailPreprocessor
+from .modeling import RetailForecaster
+from .utils import setup_logging, DataLoader
+
+# Version information
+__version__ = '1.0.0'
+
+# Package metadata
+__author__ = 'Pavan Kakarrot'
+__description__ = 'Retail Sales Forecasting Package'
